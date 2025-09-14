e6199985064a509d798b11a63c411d3caa652110
Rubén Quintas Alonso <untalrubi@gmail.com>
2025-09-14 13:48:15 +0200

    feat: Print file names from a directory.

--- src/main/java/edu/teis/Main.java
+++ src/main/java/edu/teis/Main.java
@@ -1,10 +1,27 @@
 package edu.teis;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+    public static void main(String[] args) throws IOException {
 
+        System.out.println("Introduce un directorio:");
+        Scanner scanner = new Scanner(System.in);
+        String dirRoute = scanner.nextLine();
+        Path path = Path.of(dirRoute);
+
+        System.out.println("Contenido de " + dirRoute);
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path file : stream) {
+
+                System.out.println(file.getFileName());
+            }
+        }
+    }
 }
